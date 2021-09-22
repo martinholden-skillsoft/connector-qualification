b1a4a904d96853fa4d0bb788d5fbfc859f80910b
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2021-09-22 18:28:49 +0100

    changelog

--- build/connector-qualification.bundle.js
+++ build/connector-qualification.bundle.js
@@ -1,4 +1,4 @@
-/*! connector-qualification.bundle.js - v0.0.0 - 2021-09-22 */
+/*! connector-qualification.bundle.js - v1.0.0 - 2021-09-22 */
 "use strict";
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
